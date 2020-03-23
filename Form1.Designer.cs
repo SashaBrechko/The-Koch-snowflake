dbe93f00d779f9d9088106cecc541acfeddde7dd
SashaBrechko <brechko.alexandra@gmail.com>
2020-03-23 23:42:14 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Draw = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btn_Draw
+            // 
+            this.btn_Draw.AllowDrop = true;
+            this.btn_Draw.BackColor = System.Drawing.Color.White;
+            this.btn_Draw.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Draw.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Draw.ForeColor = System.Drawing.Color.SlateGray;
+            this.btn_Draw.Location = new System.Drawing.Point(562, 591);
+            this.btn_Draw.Name = "btn_Draw";
+            this.btn_Draw.Size = new System.Drawing.Size(310, 38);
+            this.btn_Draw.TabIndex = 0;
+            this.btn_Draw.Text = "Draw the Koch snowflake";
+            this.btn_Draw.UseVisualStyleBackColor = false;
+            this.btn_Draw.Click += new System.EventHandler(this.btn_Draw_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.AllowDrop = true;
+            this.btn_Close.BackColor = System.Drawing.Color.White;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Close.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Close.ForeColor = System.Drawing.Color.SlateGray;
+            this.btn_Close.Location = new System.Drawing.Point(12, 591);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(88, 38);
+            this.btn_Close.TabIndex = 1;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.SlateGray;
+            this.button1.Location = new System.Drawing.Point(106, 591);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Draw triangle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(480, 603);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(76, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(884, 641);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Draw);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Koch snowflake";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Draw;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            Graphics gr = CreateGraphics();
+            Pen p = new Pen(Color.White, 2);
+            Pen p2 = new Pen(Color.LightSlateGray, 2);
+
+            gr.DrawLine(p, 300, 400, 450, 200);
+            gr.DrawLine(p, 450, 200, 600, 400);
+            gr.DrawLine(p, 600, 400, 300, 400);
+
+            int i = comboBox1.SelectedIndex + 1;
+
+
+        }
+
+        private void btn_Draw_Click(object sender, EventArgs e)
+        {
+            float x1 = 300;
+            float x2 = 450;
+            float x3 = 600;
+
+            float y1 = 400;
+            float y2 = 200;
+            float y3 = 400;
+
+
+        }
+
+        public void KochSnoflake(float x1, float x2, float x3, float y1, float y2, float y3, int i)
+        {
+            float P = 3/10;
+
+            while (i > 0)
+            {
+                float nx1 = x1 + (x2 - x1) * P;
+                float nx2 = x2 + (x3 - x2) * P;
+                float nx3 = x3 + (x1 - x3) * P;
+
+                float ny1 = y1 + (y2 - y1) * P;
+                float ny2 = y2 + (y3 - y2) * P;
+                float ny3 = y3 + (y1 - y3) * P;
+
+                
+
+                x1 = nx1;
+                x2 = nx2;
+                x3 = nx3;
+
+                y1 = ny1;
+                y2 = ny2;
+                y3 = ny3;
+
+                i--;
+            }
+
+        }
+
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = comboBox1.SelectedIndex + 1;
+        }
+    }
+}
